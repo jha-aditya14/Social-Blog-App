347bb6ea3de925114f250bbcbf1722ecee4312d1
jha-aditya14 <137380998+jha-aditya14@users.noreply.github.com>
2023-06-22 22:27:29 +0530

    Cloud DB Changes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Social-Blog-App
+
+
+$env:DATABASE_URL = "postgres://blog:tM28cTbZTWLEWXl8Y6zvvLx7vyJiHhqc@dpg-cia7btmnqql8alje2jbg-a.oregon-postgres.render.com/socialblogapp"
--- socialBlog/socialBlog/settings.py
+++ socialBlog/socialBlog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,22 +77,32 @@ WSGI_APPLICATION = 'socialBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+#======== local db ===========
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
-        'USER':'blog',
-        'PASSWORD': '9560',
-        'HOST':'localhost'
-    }
-}
 
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'blog',
+#         'USER':'blog',
+#         'PASSWORD': '9560',
+#         'HOST':'localhost'
+#     }
+# }
+
+#========== External DB ============
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
