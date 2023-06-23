cebf742d49d3389bed1591fda52c5d59fcbc6490
jha-aditya14 <137380998+jha-aditya14@users.noreply.github.com>
2023-06-23 11:58:10 +0530

    Requrements changes (#11)
    * Update requirements.txt
    
    * Update sign-in.html
    
    * static root

--- socialBlog/settings.py
+++ socialBlog/settings.py
@@ -139,7 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'assets/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
+STATIC_ROOT = 'assets/'
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
